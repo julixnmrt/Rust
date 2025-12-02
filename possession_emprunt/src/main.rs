d83935c9fa518af51e1216b1eeba636c61d67ef6
Julian Martineau <julian.martineau@grenoble-inp.org>
2025-12-01 23:49:15 -0500

    possession exemple

---
+++ possession_emprunt/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    possession_chaine()
+}
+fn possession_chaine(){
+    let chaine = String::from("ENI");
+    let chaine2 = chaine;
+
+    print!("{}", chaine2);
+    //print!("{}", chaine);
+
+}
